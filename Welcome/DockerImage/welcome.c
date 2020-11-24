ec467a4e6e7cc146520ed49142625049e589f1de
Sravani Avusherla <sravs1603@gmail.com>
2020-11-24 22:47:19 +0100

    Welcome: flush logs for printf

--- Welcome/DockerImage/welcome.c
+++ Welcome/DockerImage/welcome.c
@@ -2,4 +2,11 @@
 void main()
 {
    printf("Welcome cheema, keep rolling in the container !!\n");
+<<<<<<< HEAD
+=======
+   fflush(stdout);
+   while(1) {
+     
+   }
+>>>>>>> Welcome: flush logs for printf
 }
