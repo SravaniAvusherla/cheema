c39714accfabec059c620198b0b23e52316bdbbf
Sravani Avusherla <sravs1603@gmail.com>
2020-11-27 23:06:25 +0100

    Fixed dirty state of the file from a rebase

--- welcome/welcome.c
+++ welcome/welcome.c
@@ -2,11 +2,8 @@
 void main()
 {
    printf("Welcome cheema, keep rolling in the container !!\n");
-<<<<<<< HEAD
-=======
    fflush(stdout);
    while(1) {
      
    }
->>>>>>> Welcome: flush logs for printf
 }
